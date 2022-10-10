2343154347139b71fc4a53381f2316742bc9802e
joch1175 <joseph@hellohornet.com>
2022-10-10 00:48:55 -0400

    swapYetiTexturePath v2

--- swapYetiTexturePath/swapYetiTexturePath.py
+++ swapYetiTexturePath/swapYetiTexturePath.py
@@ -1,18 +1,17 @@
+# swapYetiTexturePath.py
+
+# Powered by: John Kalaigian / Joseph Chen
+# Date 10 Oct 2022
+# Version 1.0
+# Contact: joseph@hellohornet.com / joseph50422@gmail.com
+
 import pymel.core as pm
-import gc
 import os
 yeti = pm.ls(type='pgYetiMaya')
-path = []
 for each in yeti:
     textureNodes = pm.pgYetiGraph(each, listNodes=True, type='texture')
     for every in textureNodes:
         #get node path
-        path.append(os.path.dirname(pm.pgYetiGraph(each, node=every, param='file_name', getParamValue=True)).replace('\\','/'))
-        #file = os.path.basename(pm.pgYetiGraph(each, node=every, param='file_name', getParamValue=True))
+        path = os.path.dirname(pm.pgYetiGraph(each, node=every, param='file_name', getParamValue=True)).replace('\\','/')
         #set node path
-        #pm.pgYetiGraph(each, node=every, param='file_name', setParamValueString='NEW_PATH')
-list(set(path))
-
-
-del(list)
-gc.collect()
+        pm.pgYetiGraph(each, node=every, param='file_name', setParamValueString=path.replace('P:/','/mnt/prod/'))
