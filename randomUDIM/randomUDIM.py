43cab1ef6aa543ec23f877607a87c9d594e7dd40
joch1175 <joseph@hellohornet.com>
2022-10-09 23:02:20 -0400

    randomUDIM v2

--- randomUDIM/randomUDIM.py
+++ randomUDIM/randomUDIM.py
@@ -2,11 +2,23 @@ import maya.cmds as cmds
 import random as random
 import math as math
 
-UDIM_num = 3
 meshList = cmds.ls(sl=True)
+UDIM_num = 3
 
 for mesh in meshList:
-	intNumber =  int(math.floor(random.random()*UDIM_num))
-	meshFace = mesh + ".f[*]"
-	cmds.polyEditUV(meshFace, uValue = intNumber, relative = True)
-	print ("%s moved to UDIM_100%s" %(mesh, intNumber + 1))
+	meshParent = cmds.listRelatives(parent=True)
+	
+	cmds.polySeparate(mesh, constructionHistory=False)
+	compList = cmds.listRelatives(mesh, children=True)
+	
+	for comp in compList:
+		intNumber = 0
+		intNumber = int(math.floor(random.random()*UDIM_num))
+		CompFace = comp + ".f[*]"
+		cmds.polyEditUV(CompFace, uValue = intNumber, relative = True)
+		print ("%s moved to UDIM_100%s" %(comp, intNumber + 1))
+	    
+	cmds.polyUnite(mesh, constructionHistory=False, mergeUVSets=True, name=mesh)
+	cmds.parent(mesh, meshParent)
+	
+	print ("%s UDIM randomized successfully" %mesh)
