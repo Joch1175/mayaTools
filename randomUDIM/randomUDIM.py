bf5c9dd81a919f615d4fb8df5373f1325d1df6f9
joch1175 <joseph@hellohornet.com>
2022-10-09 23:02:33 -0400

    randomUDIM v3

--- randomUDIM/randomUDIM.py
+++ randomUDIM/randomUDIM.py
@@ -1,24 +1,59 @@
 import maya.cmds as cmds
 import random as random
 import math as math
+from functools import partial
 
-meshList = cmds.ls(sl=True)
-UDIM_num = 3
+def connectButtonPush(udimfield, checkUnpack, *args):
+	udimNum = cmds.intField(udimfield, query=True, value=True) 
+	meshList = cmds.ls(sl=True)
 
-for mesh in meshList:
-	meshParent = cmds.listRelatives(parent=True)
+	if (cmds.checkBox(checkUnpack, query=True, value=True)):
+		for mesh in meshList:
+			meshParent = cmds.listRelatives(mesh, parent=True)
+			meshParentChildNum = len(cmds.listRelatives(meshParent, children=True))
+			print (meshParentChildNum)
+			if(meshParentChildNum == 1):
+				temp = cmds.spaceLocator(name='tempLocator')
+				cmds.parent(temp, meshParent)
+			cmds.polySeparate(mesh, name = 'comp_#', constructionHistory=False)
+			compList = cmds.listRelatives(mesh, children=True)
+			
+			for comp in compList:
+				intNumber = int(math.floor(random.random()*udimNum))
+				compFace = comp + '.f[*]'
+				cmds.polyEditUV(compFace, uValue = intNumber, relative = True)
+				print ('%s moved to UDIM_100%s' %(comp, intNumber + 1))
+		
+			cmds.polyUnite(mesh, name=mesh, constructionHistory=False, mergeUVSets=True)
+			cmds.parent(mesh, meshParent)
+
+			if(meshParentChildNum == 1):
+				cmds.delete(temp)
+
+			print ("%s UDIM randomized successfully" %mesh)
+			
+	else:
+		for mesh in meshList:
+			intNumber =  int(math.floor(random.random()*udimNum))
+			meshFace = mesh + '.f[*]'
+			cmds.polyEditUV(meshFace, uValue = intNumber, relative = True)
+			print ("%s moved to UDIM_100%s" %(mesh, intNumber + 1))
+
+def randUDIM():
+
+	window = cmds.window( title = 'randomUDIM', iconName = 'randUDIM', widthHeight = (250, 150), sizeable = False)
+	cmds.frameLayout('Randomize settings', labelAlign ='top', borderStyle ='in')
+	cmds.rowColumnLayout(numberOfColumns = 2, columnWidth = [(1, 80), (2, 120)], columnSpacing = [(1,10), (2,10)] )
+	cmds.text(label = 'UDIM number', align = 'right')
+	udimfield = cmds.intField(value = 3)
+	cmds.setParent('..')
+
+	cmds.columnLayout(adjustableColumn=True)
+	checkUnpack = cmds.checkBox( label = 'Unpack combined meshes.\n(Cannot work with referenced object)', value = False)
+	cmds.setParent('..')
+
+	cmds.button( label = 'Randomize UDIM', command = partial(connectButtonPush, udimfield, checkUnpack))
+	cmds.showWindow()
 	
-	cmds.polySeparate(mesh, constructionHistory=False)
-	compList = cmds.listRelatives(mesh, children=True)
-	
-	for comp in compList:
-		intNumber = 0
-		intNumber = int(math.floor(random.random()*UDIM_num))
-		CompFace = comp + ".f[*]"
-		cmds.polyEditUV(CompFace, uValue = intNumber, relative = True)
-		print ("%s moved to UDIM_100%s" %(comp, intNumber + 1))
-	    
-	cmds.polyUnite(mesh, constructionHistory=False, mergeUVSets=True, name=mesh)
-	cmds.parent(mesh, meshParent)
-	
-	print ("%s UDIM randomized successfully" %mesh)
+if __name__=='__main__':
+    randUDIM()
