39d1b9bde1f87776b46142e8c6ba7a23f7089f13
joch1175 <joseph@hellohornet.com>
2023-01-10 12:13:28 -0500

    paring both GRP and GEO

--- pypeXgenBind/pypeXgenBind.py
+++ pypeXgenBind/pypeXgenBind.py
@@ -12,14 +12,23 @@ for element in ns.keys():
 #iterate through XGen splines
 for each in pm.ls(type="xgmSplineBase"):
     print('SplineBase: ' + each)
+    #get current namespace from current iterator name
+    currentNS = each.split(':')[0] + ':'
+    
     for xgen_substrate in each.inputs():
-        print('XGen Substrate Name: ' + xgen_substrate + ' with cbId: ' + xgen_substrate.cbId.get())
+       
         #now search in MDL grp only, ignoring PROXY grp, for anything that has scalp_GEO and replace search string with just GEO
-        asset_substrate = pm.ls(regex='.*MDL_GRP\|.*' + xgen_substrate.name().replace('scalp_GEO', 'GEO').split(':')[1])
-        #print asset_substrate
+        #because each.inputs() returns [namespace]:xxx_GEO or [namespace]:xxx_GRP|[namespace]:xxx_GEO
+        #replacing namespace as ".*" can make ".*MDL_GRP\|.*xxx_GRP\|.*xxx_GEO" and ".*MDL_GRP\|.*xxx_GEO" both searchable key
+        queryKey = '.*MDL_GRP\|' + xgen_substrate.name().replace('scalp_GEO', 'GEO').replace(currentNS, '.*').replace('|', '\|')
+        asset_substrate = pm.ls(regex=queryKey)
+        
+        #outputs 
+        print('Query Keyword is: ' + queryKey)
+        print('XGen Substrate Name: ' + xgen_substrate + ' with cbId: ' + xgen_substrate.cbId.get())
         print('Asset Substrate Name: ' + asset_substrate[0] + ' with cbId: ' + asset_substrate[0].cbId.get())
-    print('\n')
+        
+        #create blendshape, and set the weight as 1
+        pm.blendShape(asset_substrate[0], xgen_substrate, w=(0,1))
 
-#experimental
-pm.ls(regex='.*MDL_GRP\|.*scalp_\d+_GEO')
-pm.selected()[0].cbId.get()
+    print('\n')
