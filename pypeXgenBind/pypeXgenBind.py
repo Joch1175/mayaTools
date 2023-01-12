4f8c63bdd5a89163c3bd8d197c75e318af54aeb4
joch1175 <joseph@hellohornet.com>
2023-01-12 15:10:56 -0500

    pypeXgenBind.py: warrning message for exceptions

--- pypeXgenBind/pypeXgenBind.py
+++ pypeXgenBind/pypeXgenBind.py
@@ -12,23 +12,30 @@ for element in ns.keys():
 #iterate through XGen splines
 for each in pm.ls(type="xgmSplineBase"):
     print('SplineBase: ' + each)
+    
     #get current namespace from current iterator name
     currentNS = each.split(':')[0] + ':'
     
-    for xgen_substrate in each.inputs():
-       
-        #now search in MDL grp only, ignoring PROXY grp, for anything that has scalp_GEO and replace search string with just GEO
+    for xgen_substrate in each.inputs():    
+        print('XGen Substrate Name: ' + xgen_substrate + ' with cbId: ' + xgen_substrate.cbId.get())
+        
+        #composing the query key for searching
+        #search in MDL grp only, ignoring PROXY grp, for anything that has scalp_GEO and replace search string with just GEO
         #because each.inputs() returns [namespace]:xxx_GEO or [namespace]:xxx_GRP|[namespace]:xxx_GEO
         #replacing namespace as ".*" can make ".*MDL_GRP\|.*xxx_GRP\|.*xxx_GEO" and ".*MDL_GRP\|.*xxx_GEO" both searchable key
         queryKey = '.*MDL_GRP\|' + xgen_substrate.name().replace('scalp_GEO', 'GEO').replace(currentNS, '.*').replace('|', '\|')
-        asset_substrate = pm.ls(regex=queryKey)
-        
-        #outputs 
         print('Query Keyword is: ' + queryKey)
-        print('XGen Substrate Name: ' + xgen_substrate + ' with cbId: ' + xgen_substrate.cbId.get())
-        print('Asset Substrate Name: ' + asset_substrate[0] + ' with cbId: ' + asset_substrate[0].cbId.get())
         
-        #create blendshape, and set the weight as 1
-        pm.blendShape(asset_substrate[0], xgen_substrate, w=(0,1))
-
+        #search the binding target with queryKey
+        asset_substrate = pm.ls(regex=queryKey, type = 'transform')
+    
+        #warrning message for exceptions
+        if (len(asset_substrate)!=1):
+            pm.warning("There are " + str(len(asset_substrate)) + " result(s) match with regex " + queryKey + ":")
+            continue
+        else:
+            #create blendshape, and set the weight as 1
+            pm.blendShape(asset_substrate[0], xgen_substrate, w=(0,1))
+            print('Asset Substrate Name: ' + asset_substrate[0] + ' with cbId: ' + asset_substrate[0].cbId.get())
+            
     print('\n')
