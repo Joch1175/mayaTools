0de40a686c8aef287d088bc1f38cb88857aeb7cf
joch1175 <joseph@hellohornet.com>
2023-01-12 11:56:46 -0500

    add other yeti tools

---
+++ Yeti/convertYetiTempPath/convertYetiTempPath.py
@@ -0,0 +1,18 @@
+import pymel.core as pm
+import time
+
+#Get all yeti nodes in scene, not including groom nodes as they have no cache i/o functionality
+yeti = pm.ls(type='pgYetiMaya')
+
+#Selecting all yeti nodes before caching is the easiest way to cached 
+#Based on the Concurrent Caching docs https://support.peregrinelabs.com/support/solutions/articles/66000481611-working-with-caches#Concurrent-Caching
+pm.select(yeti, r = True)
+
+#replace the Windows path with Linux path
+for each in yeti:
+    try:
+        each.fileMode.set(1)
+        path = each.cacheFileName.get()
+        each.cacheFileName.set(path.replace('P:/','/mnt/prod/'))
+    except Exception, e:
+        continue
